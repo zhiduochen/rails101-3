53b973ac02e00b5104d5bf3c5c6b94827bbbf579
chenzhiduo <zhiduochen@gmail.com>
2017-03-16 18:26:02 +0800

    group relation

--- app/models/group.rb
+++ app/models/group.rb
@@ -4,5 +4,5 @@ class Group < ApplicationRecord
   validates :title, presence: true
 
   has_many :group_relationships
-  has_many :members, through: : group_relationships, source: :user
+  has_many :members, through: :group_relationships, source: :user
 end
